5d2152996355a994ed5055983a1298af08650883
Dan Foster <dan.foster@bristol.ac.uk>
2015-09-02 22:36:06 +0100

    Add bootstrap template

--- money/urls.py
+++ money/urls.py
@@ -20,5 +20,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^upload/$', views.upload, name='upload'),
     url(r'^admin/', include(admin.site.urls)),
 ]
--- money/views.py
+++ money/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
 
 
 def index(request):
-  return HttpResponse("Hello, world. You're at the money index.")
+  template = loader.get_template('money/index.html')
+  context = RequestContext(request, {
+  })
+  return HttpResponse(template.render(context))
+
+def upload(request):
+  template = loader.get_template('money/upload.html')
+  context = RequestContext(request, {
+  })
+  return HttpResponse(template.render(context))
